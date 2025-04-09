52ed9f46029fa4caa6fc38aeea271ab0e5bf5487
AlanLivingston <AlanLivingston.Home@gmail.com>
2025-04-09 16:24:42 -0400

    v1.0 Completed

--- pseg_parse.py
+++ pseg_parse.py
@@ -2,11 +2,53 @@
 pseg_parse.py
 """
 
+from typing import Union
 import re
 from csv import reader
 from argparse import ArgumentParser
-from datetime import datetime
+from datetime import datetime, time, timedelta
 import xlsxwriter
+
+# Peak and super off peak time periods.
+# Peak time is from 3:00 PM up to 7:00 PM except weekends.
+PEAK_START: time = time(15, 0, 0)
+PEAK_END: time = time(18, 59, 59)
+
+# Super Off-Peak is from 10:00 PM up to 5:59:59 AM including weekends.
+# Shift by two hours to make comparisons easier.
+TIME_OFFSET: timedelta = timedelta (hours = 2)
+SUPER_OFF_PEAK_START: time = (datetime(1, 1, 1, 22, 0, 0) + TIME_OFFSET).time()
+SUPER_OFF_PEAK_END: time = (datetime(1, 1, 1, 5, 59, 59) + TIME_OFFSET).time()
+
+def get_day_of_week(date: datetime) -> int:
+    """
+    Return day of week (0 = Sunday, 1 = Monday, ..., 6 = Saturday) given a datetime object.
+    """
+    return int(date.strftime('%w'))
+
+def is_peak_time(date_time_val: datetime) -> bool:
+    """
+    Returns True if date_time_val is in peak period.
+
+    Peak begins at PEAK_START and ends at PEAK_END, except Saturady and Sunday.
+    """
+
+    # Weekends aren't peak.
+    if get_day_of_week(date_time_val) in [0, 6]:
+        return False
+    
+    return (date_time_val.time() >= PEAK_START) and (date_time_val.time() <= PEAK_END)
+
+def is_super_off_peak_time(date_time_val: datetime) -> bool:
+    """
+    Returns True if date_time_val is in super_off_peak_period.
+
+    Super off peak is between 10:00 PM and 5:59:59 AM inclusive.  Adding two hours to super off peak start, super off peak end,
+    and the candidate date_time_val makes the comparisons easier, since then they all fall within the same day.
+    """
+    time_val: time = (date_time_val + TIME_OFFSET).time()
+
+    return (time_val >= SUPER_OFF_PEAK_START) and (time_val <= SUPER_OFF_PEAK_END)
 
 def add_title_cells(book, sheet) -> None:
     """
@@ -22,7 +64,6 @@ def add_title_cells(book, sheet) -> None:
     sheet.merge_range('B1:C1', 'Non-TOU Billing', merge_format)
     sheet.merge_range('E1:H1', 'Off-Peak Billing', merge_format)
     sheet.merge_range('J1:O1', 'Super Off-Peak Billing', merge_format)
-    sheet.merge_range('Q1:R1', 'Standard Billing', merge_format)
 
     sheet.merge_range('E2:F2', 'Peak', merge_format)
     sheet.merge_range('G2:H2', 'Off-Peak', merge_format)
@@ -33,9 +74,7 @@ def add_title_cells(book, sheet) -> None:
 
     sheet.write_row('A3', ['Time', 'Consumed', 'Generated', '',
                            'Consumed', 'Generated', 'Consumed', 'Generated', '',
-                           'Consumed', 'Generated', 'Consumed', 'Generated', 'Consumed', 'Generated', '',
-                           'Consumed', 'Generated'])
-
+                           'Consumed', 'Generated', 'Consumed', 'Generated', 'Consumed', 'Generated'])
 
 def pseg_parse(csv_file_name: str, xslx_file_name: str) -> None:
     """
@@ -49,16 +88,13 @@ def pseg_parse(csv_file_name: str, xslx_file_name: str) -> None:
     super_off_peak_start: datetime = datetime(2025, 1, 1, 22, 0, 0)
     super_off_peak_end: datetime = datetime(2025, 1, 2, 5, 59, 59)
 
-    workbook: Workbook = xlsxwriter.Workbook(xslx_file_name)
+    workbook: Workbook = xlsxwriter.Workbook(xslx_file_name, {'default_date_format': 'mmm dd yyyy hh:mm'})
     worksheet: Unknown | Worksheet = workbook.add_worksheet('PSEG TOU Usage')
     if worksheet is None:
         print('Error creating worksheet!')
         return
 
     add_title_cells(workbook, worksheet)
-
-    worksheet.autofit()
-    workbook.close()
 
     with open(csv_file_name) as csv_file:
         csv_reader = reader(csv_file)
@@ -72,6 +108,7 @@ def pseg_parse(csv_file_name: str, xslx_file_name: str) -> None:
         try:
             line: int = 0
             meter: list[str]
+            sheet_row: int = 3  # Last title row is 3 and worksheet cell indexing is zero based.
             for line, meter in enumerate(csv_reader):
                 gen_meter = next(csv_reader)
                 try:
@@ -85,14 +122,47 @@ def pseg_parse(csv_file_name: str, xslx_file_name: str) -> None:
                     print(f'Mismatched meters at lines [{line} - {line + 1}].')
                     return
                 
+                meter_time: datetime = datetime.strptime(meter[0], '%m/%d/%Y %I:%M:%S %p')
+                gen_meter_time: datetime = datetime.strptime(gen_meter[0], '%m/%d/%Y %I:%M:%S %p')
+
+                if meter_time != gen_meter_time:
+                    print(f'Mismatched meter times at lines [{line} - {line + 1}].')
+                    return
+                
+                # Column 0: Time
+                worksheet.write_datetime(sheet_row, 0, meter_time)
+
+                consumed: float = float(meter[kwh_col])
+                generated: float = float(gen_meter[kwh_col])
+                # Columns B-D - Non-TOU Billing: Consumed, Gen'd, and empty cell.
+                kwh_data: list[Union[float, str]] = [consumed, generated, '']
+
+                # Rate 195 Super off-peak is off-peak for rate 194
+                if is_super_off_peak_time(meter_time):
+                    # Columns E-I = Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, and empty cell.
+                    kwh_data.extend([0, 0, consumed, generated, ''])
+                    # Columns J-P - Super Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, Super Off-Peak Consumed and Gen'd, and empty cell.
+                    kwh_data.extend([0, 0, 0, 0, consumed, generated, ''])
+                else:
+                    if is_peak_time(meter_time):
+                        # Columns E-I = Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, and empty cell.
+                        kwh_data.extend([consumed, generated, 0, 0, ''])
+                        # Columns J-P - Super Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, Super Off-Peak Consumed and Gen'd, and empty cell.
+                        kwh_data.extend([consumed, generated, 0, 0, 0, 0, ''])
+                    else:
+                        # If it's neither super off-peak nor peak, then it's off-peak.
+                        # Columns E-I = Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, and empty cell.
+                        kwh_data.extend([0, 0, consumed, generated, ''])
+                        # Columns J-P - Super Off-Peak Billing - Peak Consumed and Gen'd, Off-Peak Consumed and Gen'd, Super Off-Peak Consumed and Gen'd, and empty cell.
+                        kwh_data.extend([0, 0, consumed, generated, 0, 0, ''])
+                worksheet.write_row(sheet_row, 1, kwh_data)
+                sheet_row += 1
 
         except StopIteration:
             print('Read beyond end of file.  Check that both meter and generated meter data are included.')
 
-
-
-
-
+    worksheet.autofit()
+    workbook.close()
 
 def main():
     """
